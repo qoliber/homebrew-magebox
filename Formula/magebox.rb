dea69808167637627031e606cc45cd9f1e7fef6c
Jakub Winkler <jwinkler@qoliber.com>
2025-12-16 15:01:41 +0100

    fix: correct SHA256 checksums from GitHub Actions build

--- Formula/magebox.rb
+++ Formula/magebox.rb
@@ -10,14 +10,14 @@ class Magebox < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/qoliber/magebox/releases/download/v#{version}/magebox-darwin-arm64"
-      sha256 "87c0e8142a1e79bce7d507b501fd76436b586ca542d2f81679476aba9f483541"
+      sha256 "07d3782d0b7ece28d362a7b6466f3cdfdebf1b92243e2d503c820ea2dcb2f073"
 
       def install
         bin.install "magebox-darwin-arm64" => "magebox"
       end
     else
       url "https://github.com/qoliber/magebox/releases/download/v#{version}/magebox-darwin-amd64"
-      sha256 "be7e28cf4776d4d0ab1ce9df6c325e37ddde5026303b0483f1a6fc7aa42eabd8"
+      sha256 "ab0d1acbbf636e5636ca84bff4d3634882185bd67b561cd80cb0e079681480f0"
 
       def install
         bin.install "magebox-darwin-amd64" => "magebox"
@@ -28,14 +28,14 @@ class Magebox < Formula
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/qoliber/magebox/releases/download/v#{version}/magebox-linux-arm64"
-      sha256 "7c0c7a4905a08eb7bfb2c623bf995c3e1370c5d9ad7551dbe12667c77c21b862"
+      sha256 "5d77e20f480354eadc53bfc73c3b792f67cd329582b939ef06ef80c3c74ae910"
 
       def install
         bin.install "magebox-linux-arm64" => "magebox"
       end
     else
       url "https://github.com/qoliber/magebox/releases/download/v#{version}/magebox-linux-amd64"
-      sha256 "b5d70830b4609c67a27019fd3fb38bcc6b33f52754596dc3840493f6b96ca66a"
+      sha256 "132c7fce8e219d1fccfe1544a831f84728b148ad1c88cbf0be19ed6cac238288"
 
       def install
         bin.install "magebox-linux-amd64" => "magebox"
