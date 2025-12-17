7958c7877e0af90ec035b62282492b9cc29258c4
Jakub Winkler <jwinkler@magearchitect.com>
2025-12-17 10:28:07 +0100

    chore: update to v0.16.12 with Docker provider management

--- Formula/magebox.rb
+++ Formula/magebox.rb
@@ -4,20 +4,20 @@
 class Magebox < Formula
   desc "Fast, native Magento development environment"
   homepage "https://magebox.dev"
-  version "0.16.11"
+  version "0.16.12"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/qoliber/magebox/releases/download/v#{version}/magebox-darwin-arm64"
-      sha256 "90e85e8e03fa4b9dccc5adddcd2b7d18bfef716f96dbe7bca46690479c8f911a"
+      sha256 "06c7a9350adb534146e15c15c654dd656b72b67bf138b4bb4df688d0b6f882f0"
 
       def install
         bin.install "magebox-darwin-arm64" => "magebox"
       end
     else
       url "https://github.com/qoliber/magebox/releases/download/v#{version}/magebox-darwin-amd64"
-      sha256 "fff45ed4a364ff082201296ed91261b4699e8b119d16e50b4461f9b7a6e72e59"
+      sha256 "478557d73e5cad0717bbf0facbfd097be7bc7ec253aa4c64a7b2c60ac7642741"
 
       def install
         bin.install "magebox-darwin-amd64" => "magebox"
@@ -28,14 +28,14 @@ class Magebox < Formula
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/qoliber/magebox/releases/download/v#{version}/magebox-linux-arm64"
-      sha256 "1527a2f3dfb636f5da1073598bbf09ce5884b592b7d9fb76c40c8f4c507483a6"
+      sha256 "72aa86b5436a7df10480390c337c9a5c4c665544bfd1f69daf8d1034a8dcef77"
 
       def install
         bin.install "magebox-linux-arm64" => "magebox"
       end
     else
       url "https://github.com/qoliber/magebox/releases/download/v#{version}/magebox-linux-amd64"
-      sha256 "6d943876341009f0fa1682c5330df11fa57fc15cadd0feaf43034ba71d2341b2"
+      sha256 "ed37568d0cfff0a012a8a7204ec3d3e32b993614f066a9c946c7a8816dbc3a7a"
 
       def install
         bin.install "magebox-linux-amd64" => "magebox"
