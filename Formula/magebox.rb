47f33dd110ab52a29f3833cf88dfa4df664f1747
Jakub Winkler <jwinkler@magearchitect.com>
2025-12-17 08:04:34 +0100

    chore: update to v0.16.9 with pf rules reload fix

--- Formula/magebox.rb
+++ Formula/magebox.rb
@@ -4,20 +4,20 @@
 class Magebox < Formula
   desc "Fast, native Magento development environment"
   homepage "https://magebox.dev"
-  version "0.16.8"
+  version "0.16.9"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/qoliber/magebox/releases/download/v#{version}/magebox-darwin-arm64"
-      sha256 "559ab3faf3290fb76384ac3281edfdaa14581b953c7042b3b38d35eb7d6e6f30"
+      sha256 "acc2e892646392ca146c5f86f345d06f439ddfd471d117887cfb614c9503ad95"
 
       def install
         bin.install "magebox-darwin-arm64" => "magebox"
       end
     else
       url "https://github.com/qoliber/magebox/releases/download/v#{version}/magebox-darwin-amd64"
-      sha256 "c91b6adc0e516a17de8b17d080ee903b196260e80dff4b5fb58549f93f4a0a10"
+      sha256 "7f570a6a8e655c162000b635e8822bced84581542a1644a382aba78795399f60"
 
       def install
         bin.install "magebox-darwin-amd64" => "magebox"
@@ -28,14 +28,14 @@ class Magebox < Formula
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/qoliber/magebox/releases/download/v#{version}/magebox-linux-arm64"
-      sha256 "ed22940fabb531d778c22d034a0ae57a9851a0b9368d8ab7a44f20518de6ee85"
+      sha256 "537b7d9331b201ba371d8c290f6af744260947ec8cd65e1790c98484664dacc1"
 
       def install
         bin.install "magebox-linux-arm64" => "magebox"
       end
     else
       url "https://github.com/qoliber/magebox/releases/download/v#{version}/magebox-linux-amd64"
-      sha256 "694c529eebd47ac7f6571c8ee910748f56791b9ee093daa62d27c5b1941bbae9"
+      sha256 "0627249410140b0fde1f99a8490f03297fac6e314e6823647786b491d3d555f6"
 
       def install
         bin.install "magebox-linux-amd64" => "magebox"
