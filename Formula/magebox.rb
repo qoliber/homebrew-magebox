bd578270cb365ac5a20f68cf48162cfcdbb3135f
peterjaap <peterjaap@elgentos.nl>
2026-03-29 10:54:57 +0200

    Fix formula audit errors for multi-platform installs
    Replace `if Hardware::CPU.arm?` with `on_arm`/`on_intel` DSL blocks
    and move `def install` to class level to fix "Do not define methods
    in blocks" brew audit errors.

--- Formula/magebox.rb
+++ Formula/magebox.rb
@@ -8,39 +8,34 @@ class Magebox < Formula
   license "MIT"
 
   on_macos do
-    if Hardware::CPU.arm?
+    on_arm do
       url "https://github.com/qoliber/magebox/releases/download/v#{version}/magebox-darwin-arm64"
       sha256 "bfa660730b006ceb45ecbffc35ae7b0253d8a641de4fd4232c27a96b397a8b40"
-
-      def install
-        bin.install "magebox-darwin-arm64" => "magebox"
-      end
-    else
+    end
+    on_intel do
       url "https://github.com/qoliber/magebox/releases/download/v#{version}/magebox-darwin-amd64"
       sha256 "ed36ada25e5de310a225caa0e2cce2579d4264464aa7cfd6c6e023d2db519170"
-
-      def install
-        bin.install "magebox-darwin-amd64" => "magebox"
-      end
     end
   end
 
   on_linux do
-    if Hardware::CPU.arm?
+    on_arm do
       url "https://github.com/qoliber/magebox/releases/download/v#{version}/magebox-linux-arm64"
       sha256 "83c046c34bc2cc80807149b3186215ea1ec8d9e940344ff28b41f9c9798f355a"
-
-      def install
-        bin.install "magebox-linux-arm64" => "magebox"
-      end
-    else
+    end
+    on_intel do
       url "https://github.com/qoliber/magebox/releases/download/v#{version}/magebox-linux-amd64"
       sha256 "70ea51a13825a885252f858645f8d5edc5e6487b09e08e0e2d1f29d7c564d1e0"
-
-      def install
-        bin.install "magebox-linux-amd64" => "magebox"
-      end
     end
+  end
+
+  def install
+    suffix = if OS.mac?
+               Hardware::CPU.arm? ? "darwin-arm64" : "darwin-amd64"
+             else
+               Hardware::CPU.arm? ? "linux-arm64" : "linux-amd64"
+             end
+    bin.install "magebox-#{suffix}" => "magebox"
   end
 
   test do
